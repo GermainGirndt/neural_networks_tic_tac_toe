fca0d9c077e595a6ea6e27524cfc56ad0df91257
Germain Girndt <germaingirndt@outlook.com>
2023-11-07 14:59:29 +0100

    Removed redudant code

--- src/Move/Move.py
+++ src/Move/Move.py
@@ -9,23 +9,18 @@ class Move:
     checker: type[Checker] = None
     position: type[BoardPosition] = None
     board: type[Board] = None
-    already_executed: bool = False
 
     def __init__(self, checker: type[Checker], position: type[BoardPosition], board: type[Board]) -> None:
         self.checker = checker
         self.position = position
         self.board = board
-        self.already_executed = False
         self.validate_tile_is_free()
 
     def execute(self):
-        if self.already_executed:
-            raise RuntimeError('This move was already executed')
         self.validate_tile_is_free()
 
         tile: type[Tile] = self.board.get_tile(self.position)
         tile.put_match(self.checker)
-        self.already_executed = True
 
     def validate_tile_is_free(self):
         tile: type[Tile] = self.board.get_tile(self.position)
