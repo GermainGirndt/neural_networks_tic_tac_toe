8089d4553cf286c9068eb6e5a1a71c2953e2668f
Germain Girndt <germaingirndt@outlook.com>
2023-11-09 11:10:49 +0100

    Refactored

--- src/Position/Position.py
+++ src/Position/Position.py
@@ -4,22 +4,23 @@ class Position:
     y = -1
 
     def __init__(self, x, y) -> None:
-
-        if type(x) != int:
-            raise TypeError(
-                f"Position: x must be an integer. Received: {type(x)} - {x}")
-
-        if type(y) != int:
-            raise TypeError(
-                f"Position: y must be an integer. Received: {type(y)} - {y}")
-
-        if x < 0:
-            raise ValueError(
-                f"Position: x must be greater than or equal to zero. Received: {x}")
-
-        if y < 0:
-            raise ValueError(
-                f"Position: y must be greater than or equal to zero. Received: {y}")
-
         self.x = x
         self.y = y
+        self.validate_position()
+
+    def validate_position(self):
+        if type(self.x) != int:
+            raise TypeError(
+                f"Position: x must be an integer. Received: {type(self.x)} - {self.x}")
+
+        if type(self.y) != int:
+            raise TypeError(
+                f"Position: y must be an integer. Received: {type(self.y)} - {self.y}")
+
+        if self.x < 0:
+            raise ValueError(
+                f"Position: x must be greater than or equal to zero. Received: {self.x}")
+
+        if self.y < 0:
+            raise ValueError(
+                f"Position: y must be greater than or equal to zero. Received: {self.y}")
